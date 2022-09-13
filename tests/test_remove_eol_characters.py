47abffa30bba808b0d657ad28d198f53417a0cb8
C1rN09 <zhaoqian@pjlab.org.cn>
2022-09-13 21:38:49 +0800

    testcase style refine

--- tests/test_remove_eol_characters.py
+++ tests/test_remove_eol_characters.py
@@ -1,13 +1,11 @@
-import codecs
-
 from pre_commit_hooks.remove_eol_characters import remove_eol, rewrite_file
 
-wrong_text = """这是一个，
+original_text = """这是一个，
 测试。
 This is a
 test
 """
-correct_text = """这是一个，测试。
+rewrote_text = """这是一个，测试。
 This is a
 test
 """
@@ -15,10 +13,10 @@ test
 
 def test_remove_eol_characters(tmp_path):
     file_path = str(tmp_path / 'test.md')
-    with codecs.open(file_path, mode='w', encoding='utf-8') as f:
-        f.write(wrong_text)
+    with open(file_path, mode='w', encoding='utf-8') as f:
+        f.write(original_text)
     strategies = [remove_eol()]
     assert rewrite_file(file_path, strategies)
-    with codecs.open(file_path, 'r', encoding='utf-8') as f:
+    with open(file_path, encoding='utf-8') as f:
         contents = f.read()
-    assert contents == correct_text
+    assert contents == rewrote_text
